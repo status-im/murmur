fb6c38302ec0b38362171df2fc0d69670ab02a72
Richard Ramos <info@richardramos.me>
2018-11-23 13:21:36 -0400

    Added asymmetric decryption logc

--- src/messages.js
+++ src/messages.js
@@ -1,4 +1,3 @@
-const rlp = require('rlp-encoding')
 const crypto = require('crypto');
 const gcm = require('node-aes-gcm');
 const elliptic = require("elliptic");
@@ -36,8 +35,85 @@ const assignDefined = (target, ...sources) => {
   return target;
 }
 
+const kdf = (hashName, z, s1, kdLen, cb) => {
+  const BlockSize = 64;
+  // --
+  const reps =  ((kdLen + 7) * 8) / (BlockSize * 8)
+  if(reps > Math.pow(2,32) - 1) {
+    cb("Data too long");
+    return;
+  }
+  
+  let counter = Buffer.from([0,0,0,1]);
+  let k = Buffer.from([]);
+
+  for(let i = 0; i <= reps; i++){
+    const hash = crypto.createHash(hashName);
+    hash.update(Buffer.from(counter));
+    hash.update(z); // 
+    hash.update(s1);
+    k = Buffer.concat([k, hash.digest()]);
+    counter[3]++;
+  }
+  
+  return k.slice(0, 32);
+}
+
+const symDecrypt = (ct, ke, mEnd) => {
+  const blSize = 16;
+  const iv = ct.slice(0, blSize);
+  const ciphertext = ct.slice(blSize);
+
+  var cipher = crypto.createDecipheriv("aes-128-ctr", ke, iv);
+  var firstChunk = cipher.update(ciphertext);
+  var secondChunk = cipher.final();
+    
+  return (Buffer.concat([firstChunk, secondChunk]));
+} 
+
+// From parity ECIES
+// Compare two buffers in constant time to prevent timing attacks.
+function equalConstTime(b1, b2) {
+  if (b1.length !== b2.length) {
+    return false;
+  }
+  var res = 0;
+  for (var i = 0; i < b1.length; i++) {
+    res |= b1[i] ^ b2[i];  // jshint ignore:line
+  }
+  return res === 0;
+}
+
 const decryptAssymetric = (key, data, cb) => {
-    throw new Error("not implemented yet");
+  const privKey = crypto.createECDH('secp256k1');
+  privKey.setPrivateKey(key);
+  
+  const z = privKey.computeSecret(data.slice(0, 65));
+
+  const k = kdf("sha256", z, Buffer.from([]), 32)
+  if(k === null) return;
+
+  const keyLen = 16;
+  const ke = k.slice(0, keyLen);
+  let km = k.slice(keyLen);
+  km = crypto.createHash("sha256").update(km).digest();
+ 
+  const hashSize = 32;
+  const mEnd = data.length - hashSize;
+  const ct = data.slice(65, mEnd);
+
+  // Message Tag
+  const messageTag = crypto.createHmac('sha256', km).update(ct).update("").digest();
+
+  if(!equalConstTime(messageTag, data.slice(mEnd))){
+    return cb("Invalid Message");
+  }
+
+  const decrypted = symDecrypt(ct, ke, mEnd);
+
+  const msgObj = parseMessage(decrypted);
+
+  cb(null, msgObj);
 }
 
 const decryptSymmetric = (topic, key, data, cb) => {
@@ -57,37 +133,40 @@ const decryptSymmetric = (topic, key, data, cb) => {
     const msg = data.slice(0, data.length - 28);
     const decrypted = gcm.decrypt(key, salt, msg, Buffer.from([]), constants.dummyAuthTag);
 
-    let start = 1;
-    const end = decrypted.plaintext.byteLength;
+    const msgObj = parseMessage(decrypted.plaintext);
 
-    let payload;
-    let pubKey;
-
-    const auxiliaryFieldSize = decrypted.plaintext.readUIntLE(0, 1) & constants.flagMask;
-
-    let auxiliaryField;
-    if(auxiliaryFieldSize !== 0) {
-      auxiliaryField = decrypted.plaintext.readUIntLE(start, auxiliaryFieldSize);
-      start += auxiliaryFieldSize;
-      payload = decrypted.plaintext.slice(start, start + auxiliaryField);
-    }
-
-    const isSigned = (decrypted.plaintext.readUIntLE(0, 1) & constants.isSignedMask) == constants.isSignedMask;
-    let signature = null;
-    if (isSigned) {
-      console.dir(decrypted.plaintext.toString())
-      signature = getSignature(decrypted.plaintext);
-      const hash = getHash(decrypted.plaintext);
-      pubKey = ecRecoverPubKey(hash, signature);
-    }
-
-    // TODO:
-    let padding = null
-
-    if(cb){
-      return cb(null, assignDefined({}, {payload, pubKey, signature, padding}));
-    }
+    cb(null, msgObj);
   });
+}
+
+const parseMessage = (message) => {
+  let start = 1;
+  const end = message.byteLength;
+
+  let payload;
+  let pubKey;
+
+  const auxiliaryFieldSize = message.readUIntLE(0, 1) & constants.flagMask;
+
+  let auxiliaryField;
+  if(auxiliaryFieldSize !== 0) {
+    auxiliaryField = message.readUIntLE(start, auxiliaryFieldSize);
+    start += auxiliaryFieldSize;
+    payload = message.slice(start, start + auxiliaryField);
+  }
+
+  const isSigned = (message.readUIntLE(0, 1) & constants.isSignedMask) == constants.isSignedMask;
+  let signature = null;
+  if (isSigned) {
+    signature = getSignature(message);
+    const hash = getHash(message);
+    pubKey = ecRecoverPubKey(hash, signature);
+  }
+
+  // TODO:
+  let padding = null
+
+  return assignDefined({}, {payload, pubKey, signature, padding});
 }
 
 const getSignature = (plaintextBuffer) => "0x" + plaintextBuffer.slice(plaintextBuffer.length - constants.signatureLength, plaintextBuffer.length).toString('hex');
@@ -109,5 +188,6 @@ const ecRecoverPubKey = (messageHash, signature) => {
 
 
 module.exports = {
-  decryptSymmetric
+  decryptSymmetric,
+  decryptAssymetric
 };
