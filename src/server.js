1285e499b640f3706a5fcbd378be24f50cc081b1
Richard Ramos <info@richardramos.me>
2019-01-23 11:29:35 -0400

    added commands to enable/disable protocols on server

--- src/server.js
+++ src/server.js
@@ -1,31 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const chalk = require('chalk');
+const program = require('commander');
 
-require('express-ws')(app);
+program
+  .version('0.1.0')
+  .option('--ws', 'Enable the WS-RPC server')
+  .option('--wsport [port]', 'WS-RPC Port (default: 8546)')
+  .option('--no-devp2p', 'Disable DEVP2P')
+  .option('--no-libp2p', 'Disable LIBP2P')
+  .parse(process.argv);
+
+let app;
+const ENABLE_WS = program.ws === true;
+const WS_PORT =  program.wsport !== undefined ? parseInt(program.wsport, 10) : 8546;
+
+if(ENABLE_WS){
+  app = express();
+  require('express-ws')(app);
+}
 
 (async () => {
 
   const Provider = require('./provider');
   const provider = new Provider();
+  const nodes = [];
 
-  // DevP2P
-  const devp2p = require('./client.js');
-  devp2p.start();
-  devp2p.connectTo({address: '127.0.0.1', udpPort: 30303, tcpPort: 30303});
+  if(program.devp2p){
+    const devp2p = require('./client.js');
+    devp2p.start();
+    devp2p.connectTo({address: '127.0.0.1', udpPort: 30303, tcpPort: 30303});
+    nodes.push(devp2p);
+  }
 
-  // LibP2P
-  const LibP2PNode = require('./libp2p-node.js');
-  const libp2p =  new LibP2PNode();
-  libp2p.start();
+  if(program.libp2p){
+    const LibP2PNode = require('./libp2p-node.js');
+    const libp2p =  new LibP2PNode();
+    libp2p.start();
+    nodes.push(libp2p);
+  }
 
 
   const Manager = require('./manager');
   const _manager = new Manager(provider, {libP2PClient: false, isBridge: true});
-  _manager.setupNodes([devp2p, libp2p]);
+  _manager.setupNodes(nodes);
   _manager.start();
 
+  if(!ENABLE_WS) return;
+  
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
 
@@ -56,6 +78,6 @@ require('express-ws')(app);
     });
   });
 
-  app.listen(8546, () => console.log(chalk.yellow('Murmur listening on port 8546!')));
+  app.listen(WS_PORT, () => console.log(chalk.yellow(`Murmur listening on port ${WS_PORT}`)));
 
 })();
