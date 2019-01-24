9db76137518274839b3772770a889702a5420c86
Richard Ramos <info@richardramos.me>
2019-01-24 09:53:58 -0400

    Refactoring murmur client options

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const Manager = require('./manager');
 
 class Murmur {  
   constructor(options) {
-    this.isLibP2PClient = options.libP2PClient || false;
-    this.isBridge = this.isLibP2PClient ? false : (options.isBridge || true);
+    this.isBridge = options.isBridge;
+    this.protocols = options.protocols || [];
     this.address = options.address || '/ip4/0.0.0.0/tcp/0';
     this.bootnodes = options.bootnodes || [];
+    this.nodes = [];
     
     this.provider = new Provider();
     this.manager = new Manager(this.provider, {
-      isLibP2PClient: this.isLibP2PClient, 
       isBridge: this.isBridge, 
       address: this.address, 
       bootnodes: this.bootnodes
@@ -18,29 +18,21 @@ class Murmur {
   }
 
   async start() {
-    const nodes = [];
+    if(this.protocols.indexOf("devp2p") > -1){
+      const devp2p = require("./clients.js");
+      devp2p.start();
+      devp2p.connectTo({address: '127.0.0.1', udpPort: 30303, tcpPort: 30303});
+      this.nodes.push(devp2p);
+    }
 
-    if(this.isLibP2PClient || this.isBridge){
+    if(this.protocols.indexOf("libp2p") > -1){
       const LibP2PNode = require('./libp2p-node.js');
-      this.libp2p = new LibP2PNode();
+      const libp2p = new LibP2PNode();
+      libp2p.start();
+      this.nodes.push(libp2p);
     }
 
-    if(!this.isLibP2PClient) {
-      this.devp2p = require('./client.js');
-    }
-
-    if(this.devp2p){
-      nodes.push(this.devp2p);
-      this.devp2p.start();
-      this.devp2p.connectTo({address: '127.0.0.1', udpPort: 30303, tcpPort: 30303});
-    }
-
-    if(this.libp2p){
-      nodes.push(this.libp2p);
-      this.libp2p.start();
-    }
-
-    this.manager.setupNodes(nodes);
+    this.manager.setupNodes(this.nodes);
     this.manager.start();
   }
 }
