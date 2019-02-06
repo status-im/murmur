e6b7e7d630822070a86f83b490ee1f93cb711d96
Richard Ramos <info@richardramos.me>
2019-02-06 10:02:05 -0400

    extract constants to file

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,9 @@
+const SHH_STATUS = 0;
+const SHH_MESSAGE = 1;
+const SHH_BLOOM = 3;
+const SHH_P2PMSG = 127;
+
+
 module.exports = {
     aesNonceLength: 12,
     dummyAuthTag: Buffer.from("11223344556677889900112233445566", 'hex'),
@@ -6,5 +12,12 @@ module.exports = {
     signatureLength: 65, // bytes,
     symKeyLength: 32, // bytes,
     keyIdLength: 32,
-    privKeyLength: 32    
+    privKeyLength: 32,
+    
+    
+    SHH_STATUS,
+    SHH_BLOOM,
+    SHH_MESSAGE,
+    SHH_P2PMSG
+  
 };
