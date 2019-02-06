c917f728740a8f64daa4dc6bd7db7facf125f8d9
Richard Ramos <info@richardramos.me>
2019-02-05 20:32:08 -0400

    added bloom filter manager class

--- src/bloom.js
+++ src/bloom.js
@@ -1,53 +1,128 @@
 const stripHexPrefix = require('strip-hex-prefix');
+const Events = require('events');
 
 const BloomFilterSize = 64;
+const syncAllowance = 10000; // 10seconds
+
+class BloomFilterManager {
+  constructor(){
+    this.bloomFilter = Buffer.alloc(BloomFilterSize);
+    this.prevFilters = [];
+    this.events = new Events();
+
+    this.events.on('updateFilter', topics => { this.updateBloomFilter(topics); });
+  }
+
+  emit(eventName, ...args){
+    this.events.emit(eventName, args);
+  }
+
+  on(eventName, cb){
+    this.events.on(eventName, cb);
+  }
+
+  match(filter){
+    if(bloomFilterMatch(this.bloomFilter, filter)) return true;
+    const prevFilters = this.prevFilters.slice();
+    for(let i = 0; i < prevFilters.length; i++){
+      if(bloomFilterMatch(prevFilters[i], filter)) return true;
+    }
+    return false;
+  }
+
+  updateBloomFilter(topics) {
+    const topicsBloomFilter = topicsToBloom(topics);   
+    if(!this.match(topicsBloomFilter)) {
+      this.setBloomFilter(bloomFilterAddition(this.bloomFilter, topicsBloomFilter));
+    }
+  }
+
+  setBloomFilter(newFilter){
+    if(newFilter.length != BloomFilterSize) throw new Error("Invalid bloom size");
+    const oldFilter = this.bloomFilter;
+    this.bloomFilter = newFilter;
+    this.prevFilters.push(oldFilter);
+    
+    this.events.emit('updated');
+
+    setTimeout(() => { // Delete old bloom filters
+      this.prevFilters = this.prevFilters.filter(x => !x.equals(oldFilter));
+    }, syncAllowance);
+  }
+
+  getBloomFilter() {
+    const b = Buffer.alloc(BloomFilterSize);
+    b.copy(this.bloomFilter);
+    return b; 
+  }
+}
+
+const bloomFilterMatch = (filter1, filter2) => {
+  if(filter1 === null || filter2 === null) return true;
+  if(filter1.length != BloomFilterSize || filter2.length != BloomFilterSize) throw new Error("Invalid bloom filter size");
+  for(let i = 0; i < BloomFilterSize; i++){
+    const a = filter1[i];
+    const b = filter2[i];
+    if((a | b) != a) return false;
+  }
+  return true;
+};
+
+const bloomFilterAddition = (filter1, filter2) => {
+  const r = Buffer.alloc(BloomFilterSize);
+  for(let i = 0; i < BloomFilterSize; i++){
+    r[i] = filter1[i] | filter2[i];
+  }
+  return r;
+};
 
 const createBloomFilter = (message) => {
-    if (message.topics && message.topics.length > 0){
-        return topicsToBloom(message.topics);
-    }
-    return topicsToBloom(message.topic);
+  if (message.topics && message.topics.length > 0){
+    return topicsToBloom(message.topics);
+  }
+  return topicsToBloom(message.topic);
 };
 
 const topicToBloom = topic => {
-    if (!Buffer.isBuffer(topic)){
-        topic = Buffer.from(stripHexPrefix(topic), 'hex');
-    }
+  if (!Buffer.isBuffer(topic)){
+    topic = Buffer.from(stripHexPrefix(topic), 'hex');
+  }
 
-    const b = Buffer.alloc(BloomFilterSize, 0);
-    const index = Array(3);
+  const b = Buffer.alloc(BloomFilterSize, 0);
+  const index = Array(3);
 
-    for (let j = 0; j < 3; j++) {
-        index[j] = parseInt(topic[j], 10);
-        if ((topic[3] & (1 << j)) != 0) {
-            index[j] += 256;
-        }
+  for (let j = 0; j < 3; j++) {
+    index[j] = parseInt(topic[j], 10);
+    if ((topic[3] & (1 << j)) != 0) {
+      index[j] += 256;
     }
+  }
 
-    for(let j = 0; j < 3; j++){
-        const byteIndex = parseInt(index[j] / 8, 10);
-        const bitIndex = parseInt(index[j] % 8, 10);
-        b[byteIndex] = (1 << bitIndex);
-    }
-    return b;
+  for(let j = 0; j < 3; j++){
+    const byteIndex = parseInt(index[j] / 8, 10);
+    const bitIndex = parseInt(index[j] % 8, 10);
+    b[byteIndex] = (1 << bitIndex);
+  }
+  return b;
 };
 
 const topicsToBloom = (topics) => {
-    let data = Buffer.alloc(BloomFilterSize, 0);
-    for (let idx = 0; idx < topics.length; idx++) {
-      const bloom = topicToBloom(topics[idx]);
-      for (let i = 0; i < BloomFilterSize; i++) {
-        data[i] = data[i] | bloom[i];
-      }
+  let data = Buffer.alloc(BloomFilterSize, 0);
+  for (let idx = 0; idx < topics.length; idx++) {
+    const bloom = topicToBloom(topics[idx]);
+    for (let i = 0; i < BloomFilterSize; i++) {
+    data[i] = data[i] | bloom[i];
     }
+  }
 
-    let combined = Buffer.alloc(BloomFilterSize, 0);
-    combined = Buffer.concat([data, combined.slice(BloomFilterSize - data.length)]);
-    return combined;
+  return data;
 };
 
 module.exports = {
-    createBloomFilter,
-    topicToBloom,
-    topicsToBloom
+  default: BloomFilterManager,
+  createBloomFilter,
+  bloomFilterAddition,
+  topicToBloom,
+  topicsToBloom,
+  bloomFilterMatch
 };
